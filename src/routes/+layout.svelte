52d9cd0342a6bbfb64333069a8fec4eed28c2f1a
Steve <ostylehouse@gmail.com>
2024-11-19 15:13:58 +1300

    style

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+
+<h1>Welcome to <strong>jamola</strong></h1>
+<slot />
+<style>
+    @font-face {
+        font-family: 'RipeApricots';
+        src: url('/RipeApricots.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+    strong {
+        font-size:300%;
+    }
+    h1, :global(button) {
+        font-family: 'RipeApricots', sans-serif;
+        background:#402;
+        padding:22px;
+        vertical-align: middle;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,8 @@
     import Call from "$lib/Call.svelte";
 
 </script>
-<h1>Welcome to jamola</h1>
 
-<main>
-    <Call />
-</main>
+<Call />
 
 <style>
-    h1 {
-        
-        background:#402;
-        padding:1em;
-    }
 </style>
